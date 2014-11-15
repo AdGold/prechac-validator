ae2bbef62c777846bbcf429d57f4f54a8975e74b
Adrian Goldwaser <agold@hotmail.com>
2014-11-15 15:07:59 +1100

    major refactoring to make it more intutive

--- validate.py
+++ validate.py
@@ -2,52 +2,55 @@ import re
 
 LEFT, RIGHT = 0,1
 other = lambda h: 1-h
-formatSS = lambda ss: ('%.3f'%ss).rstrip('0').rstrip('.')
-
-def formatThrow(throw):
-    if throw is None: return ''
-    passMod = throw[1]
-    if passMod == 'r' and throw[2] == 1: #r1 is the same as p so simplify
-        passMod = 'p'
-    elif passMod:
-        passMod += str(throw[2] or '')
-    return formatSS(throw[0]) + passMod + 'x'*throw[3]
 
 class ParseException(Exception): pass
 
-class Beat:
-    def __init__(self, left, right):
-        self.left = self.get_throw(left)
-        self.right = self.get_throw(right)
-        self.lands = [1 if self.left else 0, 1 if self.right else 0]
-        self.neg1x = [0,0]
-
-    def get_throw(self, throw):
+class Hand:
+    def __init__(self, throw):
         throwM = re.match(r'(\d+(?:\.\d*)?)(x?)(?:([rp])(\d)?)?(x?)(\*?)$', throw)
+        self.neg1x = False
         if throwM:
             th, x1, passType, passTo, x2, hurry = throwM.groups()
             if passType == 'p' and passTo is None:
                 passType, passTo = 'r', 1
-            return (float(th), passType or '', int(passTo or 0), (x1+x2 == 'x') , hurry == '*')
-        return None
-
-    def get_hand(self, side):
-        if side == LEFT:
-            return self.left
+            self.ss = float(th)
+            self.passType = passType or ''
+            self.passTo = int(passTo or 0)
+            self.x = (x1+x2 == 'x')
+            self.hurry = hurry == '*'
+            self.throw = True
         else:
-            return self.right
-
-    def __repr__(self):
-        return 'Beat(%r,%r)' % (formatThrow(self.left), formatThrow(self.right))
+            self.throw = False
 
     def __str__(self):
-        left, right = formatThrow(self.left), formatThrow(self.right)
-        return '(%s,%s)'%(left,right)
+        if not self.throw: return ''
+        passMod = self.passType
+        if passMod == 'r' and self.passTo == 1: #r1 is the same as p so simplify
+            passMod = 'p'
+        elif passMod:
+            passMod += str(self.passTo or '')
+        ss = ('%.3f'%self.ss).rstrip('0').rstrip('.')
+        return ss + passMod + 'x'*self.x
+
+    def __repr__(self):
+        return 'Hand(%r)' % str(self)
 
     def has_decimal(self):
-        left = self.left or (0.,)
-        right = self.right or (0.,)
-        return not (left[0].is_integer() and right[0].is_integer())
+        return self.throw and not self.ss.is_integer()
+
+class Beat:
+    def __init__(self, left, right):
+        self.hands = [Hand(left), Hand(right)]
+        self.lands = [int(hand.throw) for hand in self.hands]
+
+    def __repr__(self):
+        return 'Beat(%r,%r)' % (str(self.hands[LEFT]), str(self.hands(RIGHT)))
+
+    def __str__(self):
+        return '(%s,%s)' % (self.hands[LEFT], self.hands[RIGHT])
+
+    def has_decimal(self):
+        return self.hands[LEFT].has_decimal() or self.hands[RIGHT].has_decimal()
 
 class Juggler:
     def __init__(self, throws):
@@ -55,8 +58,8 @@ class Juggler:
         hand = RIGHT
         for throw in throws:
             throwRE = '\d+(?:\.\d*)?x?(?:[rp]\d?)?x?\*?'
-            sync = re.match(r'\((%s),(%s)\)!?$'%(throwRE,throwRE), throw)
-            async = re.match(r'[LR]?%s$'%throwRE, throw)
+            sync = re.match(r'\((%s),(%s)\)!?$' % (throwRE,throwRE), throw)
+            async = re.match(r'[LR]?%s$' % throwRE, throw)
             if sync:
                 left, right = sync.groups()
                 self.throws.append(Beat(left, right))
@@ -68,7 +71,7 @@ class Juggler:
                     hand = other(hand)
                 if throw[0] in 'RL':
                     hand = (throw[0] == 'R')
-                throw = throw.strip('R').strip('L')
+                throw = throw.lstrip('R').lstrip('L')
                 if hand == LEFT:
                     self.throws.append(Beat(throw, ''))
                 else:
@@ -117,53 +120,49 @@ class Prechac:
         for j,juggler in enumerate(self.jugglers):
             for i,throw in enumerate(juggler.throws):
                 ni = (i + 1) % len(juggler.throws)
-                if throw.left and throw.left[4]:
-                    throw.lands[LEFT] -= 1
-                    juggler.throws[ni].lands[LEFT] += 1
-                    juggler.throws[ni].neg1x[LEFT] = True
-                if throw.right and throw.right[4]:
-                    throw.lands[RIGHT] -= 1
-                    juggler.throws[ni].lands[RIGHT] += 1
-                    juggler.throws[ni].neg1x[RIGHT] = True
+                for HAND in (LEFT, RIGHT):
+                    if throw.hands[HAND].throw and throw.hands[HAND].hurry:
+                        throw.lands[HAND] -= 1
+                        juggler.throws[ni].lands[HAND] += 1
+                        juggler.throws[ni].hands[HAND].neg1x = True
         #Check each throw has a landing spot
         for j,juggler in enumerate(self.jugglers):
             for i,throw in enumerate(juggler.throws):
-                self.check_throw(throw.left, LEFT, j, i)
-                self.check_throw(throw.right, RIGHT, j, i)
+                self.check_throw(throw.hands[LEFT], LEFT, j, i)
+                self.check_throw(throw.hands[RIGHT], RIGHT, j, i)
 
     def check_throw(self, throw, side, juggler, beat):
-        if throw is None: return
-        ss, ptype, pto, x, hurry = throw
-        if ptype == 'r':
-            juggler_to = (juggler + pto) % self.num_jugs
-        elif ptype == 'p':
-            juggler_to = pto - 1 # convert absolute passes to 0-based index
+        if not throw.throw: return
+        if throw.passType == 'r':
+            juggler_to = (juggler + throw.passTo) % self.num_jugs
+        elif throw.passType == 'p':
+            juggler_to = throw.passTo - 1 # convert absolute passes to 0-based index
         else:
             juggler_to = juggler
-        new_ss = ss + self.jugglers[juggler].delay - self.jugglers[juggler_to].delay
+        new_ss = throw.ss + self.jugglers[juggler].delay - self.jugglers[juggler_to].delay
         if not new_ss.is_integer():
-            self.error = 'seems like a bad throw value of %f' % ss
+            self.error = 'Seems like a bad throw value of %f' % throw.ss
+            self.valid = False
             return False
         new_ss = int(new_ss)
         beat_to = (beat + new_ss) % self.period
         #if the same hand is throwing with the other juggler it will be of type 1, otherwise of type 2
         #type 1: Both right hands throwing at same time, hence crossing as expected (3p goes from R-L)
         #type 2: Right hand throwing at same time as LH, hence crossing inverse to expected (3p goes from R-R)
-        if side == LEFT:
-            diff_hand = self.jugglers[juggler_to].throws[beat].left is None
-        if side == RIGHT:
-            diff_hand = self.jugglers[juggler_to].throws[beat].right is None
-        #print('ss,new_ss,x,side,diff_hand:',ss,new_ss,x,side,diff_hand)
-        side_to = diff_hand ^ side ^ x ^ (new_ss % 2)
+        diff_hand = not self.jugglers[juggler_to].throws[beat].hands[side].throw
+
+        #print('ss,new_ss,x,side,diff_hand:',throw.ss,new_ss,throw.x,side,diff_hand)
+        side_to = diff_hand ^ side ^ throw.x ^ (new_ss % 2)
         #print('land (time,side,jug):', beat_to, side_to, juggler_to)
         #Hurry validation:
         # - add -1x on the beat after a hurry (above)
         # - if a ball is landing on a beat where the other hand is doing a hurry then skip to the next beat
         # - - unless this beat/hand also has a -1x on it
         # - not sure if this method is completely correct...
+        check = lambda hands, sidet: hands[other(sidet)].throw and hands[other(sidet)].hurry and not hands[sidet].neg1x
         max_left = self.period
         throws = self.jugglers[juggler_to].throws
-        while max_left and throws[beat_to].get_hand(other(side_to)) and throws[beat_to].get_hand(other(side_to))[4] and not throws[beat_to].neg1x[side_to]:
+        while max_left and check(throws[beat_to].hands, side_to):
             beat_to = (beat_to + 1) % self.period
             max_left -= 1
         if not self.jugglers[juggler_to].throws[beat_to].lands[side_to]:
@@ -179,6 +178,10 @@ class Prechac:
         return '<%s>' % ' | '.join(str(j) for j in self.jugglers)
 
 def tests():
+    #test basic pattern
+    assert(Prechac('<3p 3 3|3p 3 3>').valid)
+    #test brolly 552 from 1 count
+    assert(Prechac('<3p 3p 3p 4p 4p 2 3p 3p|3p 3p 3p 3p 2 3p 3p 3p>').valid)
     #test sync
     assert(Prechac('<(4x,4xp)|(4x,4xp)>').valid)
     #test hand specifiers
